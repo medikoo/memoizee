0cc9969de0ceeaea5fcef7509819e996e8ba44a0
Mariusz Nowak <mariusz@medikoo.com>
2013-10-23 18:18:39 +0200

    Memoized methods factory
    Based on `d` descriptors factory.
    It's first step to replace `method` option

---
+++ test/d.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var d       = require('d')
+  , memoize = require('../lib/regular');
+
+module.exports = function (t, a) {
+	var value = [], obj = {};
+	t = t(memoize);
+	Object.defineProperties(obj, t({
+		someFn: d(function (x, y) { a(this, obj); return x + y; },
+			{ refCounter: true,
+				dispose: function (val) { value.push(val); } })
+	}));
+
+	obj = Object.create(obj);
+	obj.someFn(3, 7);
+	obj.someFn(5, 8);
+	obj.someFn(12, 4);
+	a.deep(value, [], "Pre");
+	obj.someFn(5, 8);
+	obj.someFn.clearRef(5, 8);
+	a.deep(value, [], "Pre");
+	obj.someFn.clearRef(5, 8);
+	a.deep(value, [13], "#1");
+	value = [];
+	obj.someFn.clearRef(12, 4);
+	a.deep(value, [16], "#2");
+
+	value = [];
+	obj.someFn(77, 11);
+	obj.someFn.clearAll();
+	a.deep(value, [10, 88], "Clear all");
+};
