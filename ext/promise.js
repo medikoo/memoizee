47509f599945e8b9f6086472bb7577d9f0b594f8
Mariusz Nowak <medyk@medikoo.com>
2016-07-05 16:52:23 +0200

    Improve cache events handling

--- ext/promise.js
+++ ext/promise.js
@@ -2,22 +2,31 @@
 
 'use strict';
 
-var isPromise = require('is-promise')
+var objectMap = require('es5-ext/object/map')
+  , isPromise = require('is-promise')
   , nextTick  = require('next-tick')
 
-  , create = Object.create;
+  , create = Object.create, hasOwnProperty = Object.prototype.hasOwnProperty;
 
 require('../lib/registered-extensions').promise = function (ignore, conf) {
-	var cache = create(null);
+	var waiting = create(null), cache = create(null);
 
 	// After not from cache call
 	conf.on('set', function (id, ignore, promise) {
 		if (!isPromise(promise)) return; // Non promise result, ignore;
-		var onSuccess = function () {
-			cache[id] = promise;
-			conf.emit('setasync', id, 1);
+		waiting[id] = 1;
+		var onSuccess = function (result) {
+			var count = waiting[id];
+			if (!count) return; // deleted from cache before resolved
+			delete waiting[id];
+			cache[id] = result;
+			conf.emit('setasync', id, count);
 		};
-		var onFailure = function () { conf.delete(id); };
+		var onFailure = function () {
+			if (!waiting[id]) return; // deleted from cache before resolved
+			delete waiting[id];
+			conf.delete(id);
+		};
 		if (typeof promise.done === 'function') {
 			// Optimal promise resolution
 			promise.done(onSuccess, onFailure);
@@ -29,25 +38,30 @@ require('../lib/registered-extensions').promise = function (ignore, conf) {
 
 	// From cache (sync)
 	conf.on('get', function (id, args, context) {
+		if (waiting[id]) {
+			++waiting[id]; // Still waiting
+			return;
+		}
 		conf.emit('getasync', id, args, context);
 	});
 
 	// On delete
 	conf.on('delete', function (id) {
-		var result;
-		// If false, we don't have value yet, so we assume that intention is not
-		// to memoize this call. After value is obtained we don't cache it but
-		// gracefully pass to callback
-		if (!cache[id]) return;
-		result = cache[id];
+		if (waiting[id]) {
+			delete waiting[id];
+			return; // Not yet resolved
+		}
+		if (!hasOwnProperty.call(cache, id)) return;
+		var result = cache[id];
 		delete cache[id];
-		conf.emit('deleteasync', id, result);
+		conf.emit('deleteasync', id, [result]);
 	});
 
 	// On clear
 	conf.on('clear', function () {
 		var oldCache = cache;
 		cache = create(null);
-		conf.emit('clearasync', oldCache);
+		waiting = create(null);
+		conf.emit('clearasync', objectMap(oldCache, function (data) { return [data]; }));
 	});
 };
